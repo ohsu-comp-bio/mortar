666a12285d2460b84e83fc67bbd3210c0d53abb3
Alex Buchanan <buchanae@ohsu.edu>
2018-02-06 18:38:59 -0800

    cmd/version: add version command

---
+++ cmd/version.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/ohsu-comp-bio/mortar/version"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+  vCmd := cobra.Command{
+    Use: "version",
+    Run: func(cmd *cobra.Command, args []string) {
+      fmt.Println(version.String())
+    },
+  }
+  cmd.AddCommand(&vCmd)
+}
